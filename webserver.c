7e4cb6a6efaa4af2143756284f6915b221845cf0
Kunal Singh <kunalsin9h@gmail.com>
2024-07-05 18:14:06 +0530

    baisc server

--- webserver.c
+++ webserver.c
@@ -3,10 +3,11 @@
 #include <string.h>
 
 #include <sys/socket.h>
+#include <unistd.h>
 #include <netinet/in.h>
 
 #define APP_MAX_BUFFER 1024
-#define PORT 8080
+#define PORT 1999
 
 int main(){
     // Server and Client File Descriptors
@@ -21,6 +22,7 @@ int main(){
     char buffer[APP_MAX_BUFFER] = {0};
 
     // Server Socked
+    // SOCK_STREAM -> TCP
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("Socked creating failed");
         exit(EXIT_FAILURE);
@@ -35,6 +37,39 @@ int main(){
         exit(EXIT_FAILURE);
     }
 
+    // Creates the queues
+    // Listen for clients, with 10 backlog (10 connection in accept queue)
+    if (listen(server_fd, 10) < 0) {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    while(1){
+        printf("\nWaiting for a connection...\n");
+
+        // Accept a client connection
+        if ((client_fd = accept(server_fd, (struct sockaddr*)&address,(socklen_t*)(&address_len))) < 0) {
+             perror("Accpet failed");
+             exit(EXIT_FAILURE);
+        }
+
+        // read data from OS receive buffer to the application (buffer)
+        read(client_fd, buffer, APP_MAX_BUFFER);
+
+        printf("%s\n", buffer);
+
+        char *http_response = "HTTP/1.1 200 OK\r\n"
+                              "Content-Type: text/plain\r\n"
+                              "Content-Length: 12\r\n"
+                              "\r\n"
+                              "Hello World!";
+
+        write(client_fd, http_response, strlen(http_response));
+
+        close(client_fd);
+    }
+
 
     return 0;
 }
+
