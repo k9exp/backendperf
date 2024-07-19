c72c3e09381dd1cfb85bb9424c95a687b74bed16
Kunal Singh <kunalsin9h@gmail.com>
2024-07-19 13:24:01 +0530

    some modification

--- webserver.c
+++ webserver.c
@@ -61,7 +61,7 @@ int main() {
 
     char *http_response = "HTTP/1.1 200 OK\r\n"
                           "Content-Type: text/plain\r\n"
-                          "Content-Length: 12\r\n"
+                          "Content-Length: 16\r\n"
                           "\r\n"
                           "Hello World!\r\n";
 
